480fc8761fde60b13382fd68b8a40f9ee0ff00f0
Hermann Himmelbauer <hermann@qwer.tk>
2024-09-13 19:30:34 +0200

    Course ready

--- 17-profiling-handson.py
+++ 17-profiling-handson.py
@@ -1,7 +1,40 @@
 # We have some inefficient code in 17-profiling_code.py
-# Let's check that out
+# Let's have a look at what it does
 
-# First we use cProfile from the command line
+# First guess: Which function do you think will take longest?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# To analyze, first we use cProfile from the command line
 python -m cProfile -s cumulative 17-profiling_code.py
 
 # The result is hard to read.
@@ -18,4 +51,118 @@ kernprof -l -v 17-profiling_code.py
 
 # The result is better, we see that the real problem is unique_data()
 # Let's look into it by moving @profile to this function
+# O.k., something is very slow here.
+# -> Any suggestions how to speed up the code?
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# -> Use a set() instead of the loop:
+def remove_duplicates(data):
+    unique_data = list(set(data))
+    return unique_data
+
+# -> Wow, this speeds up things a lot
+# Now the slowest function is compute_average()
+# -> Any suggestions how to speedup things there?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Well, just use the built-in sum() function, which will hopefully be faster
+def compute_average(data):
+    total = sum(data)
+    average = total / len(data)
+    return average
+
+
+# Interestingly, the sorting is still fast. Nevertheless, any idea to simplify and
+# speedup this function?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Python provides the sorted() function just for that -> much easier and faster.
+
+def sort_data(data):
+    sorted_data = sorted(data)
+    return sorted_data
